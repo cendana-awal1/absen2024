997031b4242b612358371609997a8256e9f1ca8f
awal-cendana <awal-cendana1@outlook.com>
2024-07-23 05:36:24 +0000

    3

--- src/lib/KelasA.svelte
+++ src/lib/KelasA.svelte
@@ -1,20 +1,21 @@
 <script>
-
+	export let item;
 </script>
 
-<!-- container tilwindcss -->
+<!-- Card Student -->
 <div class="flex flex-col items-center justify-center w-64 h-96 bg-white rounded-lg shadow-lg p-6 dark:bg-surface-dark dark:shadow-gray-600">
- <div class="grid grid-cols-4 gap-4">
-    <div class="flex flex-col items-center justify-center w-64 h-96 bg-white rounded-lg shadow-lg p-6 dark:bg-surface-dark dark:shadow-gray-600">
-        <img class="w-24 h-24 rounded-full mx-auto mb-4" src="https://picsum.photos/200" alt="Student">
-      
-        <div class="w-24 h-24 mx-auto mb-4">
-          <svg class="fill-current text-gray-400" viewBox="0 0 24 24">
-            <path d="M10 6.5c-2.2 0-4 1.8-4 4s1.8 4 4 4c2.2 0 4-1.8 4-4s-1.8-4-4-4zm0 7c-3.3 0-6 2.7-6 6s2.7 6 6 6c3.3 0 6-2.7 6-6s-2.7-6-6-6zm10.5-6.5c-2.2 0-4 1.8-4 4s1.8 4 4 4c2.2 0 4-1.8 4-4s-1.8-4-4-4zm0 7c-3.3 0-6 2.7-6 6s2.7 6 6 6c3.3 0 6-2.7 6-6s-2.7-6-6-6zM9 11h-2v2h2v-2zM15 11h-2v2h2v-2z" />
-          </svg>
-        </div>
-      
-        <h3 class="text-lg font-medium text-gray-900 dark:text-white">Student Name</h3>
-      </div>
- </div>
+  <img class="w-24 h-24  rounded-full mx-auto mb-4 image" src="https://drive.google.com/thumbnail?id={item.foto}" alt="Student">
+
+  <div class="w-24 h-24 mx-auto mb-4">
+    <img
+					src="https://api.qrserver.com/v1/create-qr-code/?data={item.nama}&amp;size=120x120"
+					alt={item.nama}
+				/>
+  </div>
+
+  <h3 class="text-lg font-medium text-gray-900 dark:text-white">{item.nama}</h3>
+  <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    {item.Kelas}
+  </button>
 </div>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script>
-	import KelasA from "../lib/KelasA.svelte";
-
-
+	import KelasA from '../lib/KelasA.svelte';
+	import data from '../lib/data.json';
 </script>
 
-<KelasA/>
+<!-- container tilwindcss -->
+<div class="justify-between grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:gap-12 max-w-6xl mx-auto px-4 py-12">
+	<div class="grid grid-cols-4 gap-2 mb-3">
+		{#each data as item}
+			<KelasA {item} />
+		{/each}
+	</div>
+</div>
