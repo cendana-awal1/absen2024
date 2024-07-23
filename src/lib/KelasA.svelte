fd40a122db46f2822c870e350657b2b5b8c09230
awal-cendana <awal-cendana1@outlook.com>
2024-07-23 04:44:00 +0000

    2

---
+++ src/lib/KelasA.svelte
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<!-- container tilwindcss -->
+<div class="flex flex-col items-center justify-center w-64 h-96 bg-white rounded-lg shadow-lg p-6 dark:bg-surface-dark dark:shadow-gray-600">
+ <div class="grid grid-cols-4 gap-4">
+    <div class="flex flex-col items-center justify-center w-64 h-96 bg-white rounded-lg shadow-lg p-6 dark:bg-surface-dark dark:shadow-gray-600">
+        <img class="w-24 h-24 rounded-full mx-auto mb-4" src="https://picsum.photos/200" alt="Student">
+      
+        <div class="w-24 h-24 mx-auto mb-4">
+          <svg class="fill-current text-gray-400" viewBox="0 0 24 24">
+            <path d="M10 6.5c-2.2 0-4 1.8-4 4s1.8 4 4 4c2.2 0 4-1.8 4-4s-1.8-4-4-4zm0 7c-3.3 0-6 2.7-6 6s2.7 6 6 6c3.3 0 6-2.7 6-6s-2.7-6-6-6zm10.5-6.5c-2.2 0-4 1.8-4 4s1.8 4 4 4c2.2 0 4-1.8 4-4s-1.8-4-4-4zm0 7c-3.3 0-6 2.7-6 6s2.7 6 6 6c3.3 0 6-2.7 6-6s-2.7-6-6-6zM9 11h-2v2h2v-2zM15 11h-2v2h2v-2z" />
+          </svg>
+        </div>
+      
+        <h3 class="text-lg font-medium text-gray-900 dark:text-white">Student Name</h3>
+      </div>
+ </div>
+</div>
